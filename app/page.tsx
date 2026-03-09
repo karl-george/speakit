2cfde565906550659e370ffc3bc05aa8d9ba2c78
Karl George <karlgeorge87@gmail.com>
2026-03-09 11:10:25 +0100

    Create homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import HeroSection from "@/components/HeroSection";
+import BookCard from "@/components/BookCard";
+import {sampleBooks} from "@/lib/constants";
+
 const Page = () => {
     return (
-        <div>
-        </div>
+        <main className="wrapper container">
+            <HeroSection />
+
+            <div className="library-books-grid">
+                {sampleBooks.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug}/>
+                ))}
+            </div>
+        </main>
     )
 }
 export default Page
